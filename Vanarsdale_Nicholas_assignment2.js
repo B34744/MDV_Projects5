a89021a7a9de87f4912b9cfa3e79e6a63cd59d41
Nicholas <nicholasvanarsdale@gmail.com>
2013-10-07 20:16:52 -0700

    more work done project two

--- Vanarsdale_Nicholas_assignment2.js
+++ Vanarsdale_Nicholas_assignment2.js
@@ -11,27 +11,13 @@ var Eat = "\"Duck\"Food";
 var NumOfseeds = 10;
 var HasFood = true;
 var HasFriends = false;
-//outputs
- 
-console.log(MyDuck + " will swim across the pond for" + Eat + ". ");
-console.log(MyDuck + " will eat " + NumOfseeds + " seeds.");
-console.log("It is " + HasFood + " that " + MyDuck + " has food."); 
-console.log(MyDuck + " will only eat one type of " + Eat + "!");
 
+
+prompt(HasFriends);
 HasFriends = confirm(MyDuck + " Is hungry for food right now.");
-
-if (HasFood === false){
-	if (NumOfseeds < 100){
-		NumOfseeds = NumOfseeds * 50;
-		console.log("If " + MyDuck + " eat's " + NumOfseeds + " seeds he will be happy. ");
+if (HasFriends === false){
+		console.log("If " + MyDuck + " eat's seeds he will be happy. ");
 	} else { 
-		console.log(MyDuck + " will be sad! ");
-	}
-} else { 
-	if (HasFriends === true){
 		console.log(MyDuck + " Is a Happy duck");
-	} else {
-		console.log(MyDuck + " will eat his " + Eat );
 	}
 	
-}
