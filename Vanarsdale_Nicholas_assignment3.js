703ffcf10a241cb16f7cffbb3e338cd18388f61d
Nicholas <nicholasvanarsdale@gmail.com>
2013-10-16 22:38:20 -0700

    working with project the same project trying to master it.

---
+++ Vanarsdale_Nicholas_assignment3.js
@@ -0,0 +1,89 @@
+//alert("JavaScript works!");
+
+// Nicholas Vanarsdale 
+
+// SDI-1310
+
+// Assignment 3
+
+//global variables
+var MyDuck = "Jeff";
+var Eat = "\"Duck\"Food";
+var NumOfseeds = 10;        
+var HasMoreFood = 40;
+var HasFood = true;
+var HasFriends = false;
+
+
+// prompt
+        prompt(HasFriends);
+        
+        
+//procedure
+        var division = function(){HasMoreFood/NumOfseeds;
+        console.log( MyDuck + " eat's a lot of " + Eat);
+};division();
+
+//confirm
+HasFriends = confirm(MyDuck);
+if (HasFriends === false){
+             console.log("If " + MyDuck + " eat's seeds   he will be happy. "); 
+        } else { 
+          console.log(MyDuck + " Is a Happy duck");      
+        };
+        
+        
+        // number function        
+
+var Quacks = function(type){ 
+var TotalSeeds = 5
+var        Seeds;
+        if (type < Seeds) {
+                
+                return;
+}
+Seeds = Math.floor(type / Seeds);
+return Seeds;
+};
+var GetQuacks = Quacks(9);
+console.log("little Quacks");{
+
+
+for ( i = 1; i < NumOfseeds; i++)
+console.log(HasMoreFood + " swim's across the pond");
+
+};
+
+//String Function
+var CheckString = function(val) {
+        var Strlen = val.length;
+        for (var i = 0; i < Strlen; i++) {
+                if (val.substring(i, i+1) === "-") {
+                        return i;
+                };
+        };
+};
+console.log("How many fish is " + MyDuck + " eating " + CheckString('123456-7890'));
+
+//Boolean Function
+var Fish = function(type){ 
+var TotalFish = 10
+var        fishes;
+        if (type < fishes) {
+                
+return;                
+}
+fishes = Math.floor(type / fishes);
+return fishes;
+};
+
+
+// Can not figure out why while loop is not working ?
+   var friends=["Tim","Jack","Ben"];
+var i=0;
+while (friends[i])
+{
+console.log(friends[i] );
+i++;
+};                           
+
