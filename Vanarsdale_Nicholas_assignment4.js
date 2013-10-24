7cc3ff7616b8bb963b50dbf4ef29d715152f8a68
Nicholas <nicholasvanarsdale@gmail.com>
2013-10-24 16:39:19 -0700

    one more

--- Vanarsdale_Nicholas_assignment4.js
+++ Vanarsdale_Nicholas_assignment4.js
@@ -2,12 +2,18 @@
 
 // Number Function
 
+//The number of days in between two dates.
+var oneDay = 24*60*60*1000; 
+var firstDate = new Date(2013,01,20);
+var secondDate = new Date(2013,02,30);
 
+var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
 
 
 
 // string Function
 
+//Does the string follow a pattern like a phone number?
 var Phone = function(val) {
 		if (val.substring  === "-") {
 			return true
@@ -18,6 +24,7 @@ var Phone = function(val) {
 	};
 
 
+//does the string follow a pattern like an email address?
 var Email = function(val) {
 	if (val.IndexOf === "aaa@bbb.ccc") {
 		return true
@@ -26,6 +33,11 @@ var Email = function(val) {
 	}
 	
 };
+
+
+
+
+
 
 // array Function
 
@@ -41,6 +53,7 @@ var Email = function(val) {
 
 
 //functioon calls
-//console.log();
+console.log(diffDays);
 console.log("Does a string follow an email " + Email("aaa@bbb.ccc"));
 console.log("Does a string follow a phone number " + Phone("123-456-7890"));
+console.log();
