99bc2fa56a56ca35383e56f370421588cba8cb32
Nicholas <nicholasvanarsdale@gmail.com>
2013-10-23 22:36:12 -0700

    First function

---
+++ Vanarsdale_Nicholas_assignment4.js
@@ -0,0 +1,40 @@
+// alert("JavaScript works!");
+
+// Number Function
+
+var Phone = function(val) {
+	var strLen = val.length;
+		if (val.substring  === "-" ) {
+			return true
+		} else {
+			return false
+		}
+	
+	};
+
+
+
+
+
+// string Function
+
+
+
+
+// array Function
+
+
+
+
+
+
+
+
+
+
+
+
+//functioon calls
+//console.log();
+
+console.log("Does a string follow a phone number " + Phone("123-456-7890"));
