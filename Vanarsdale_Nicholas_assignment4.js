81b811d47894de617cd7333b7b2054ae60b61cbb
Nicholas <nicholasvanarsdale@gmail.com>
2013-10-24 17:07:35 -0700

    another one down.

--- Vanarsdale_Nicholas_assignment4.js
+++ Vanarsdale_Nicholas_assignment4.js
@@ -10,6 +10,11 @@ var secondDate = new Date(2013,02,30);
 var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
 
 
+//Format a number to use a specific number of decimal places as for money.
+var pi = parseFloat(3.14159);
+var dec = pi.toFixed(10);
+dec = dec.substring(0, dec.length-7);
+
 
 // string Function
 
@@ -56,4 +61,4 @@ var Email = function(val) {
 console.log(diffDays);
 console.log("Does a string follow an email " + Email("aaa@bbb.ccc"));
 console.log("Does a string follow a phone number " + Phone("123-456-7890"));
-console.log();
+console.log(dec);
